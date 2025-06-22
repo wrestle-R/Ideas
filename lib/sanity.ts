3a5fe0bcb7b8881de8169cfccc1aa3054c5a173b
“Russel <russeldanielpaul@gmail.com>
2025-06-22 16:26:04 +0530

    fixxed comment delays

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2025-06-17'
 })
 
